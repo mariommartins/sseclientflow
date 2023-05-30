9a1faead21a72b4de4feb599ec2f66067325252a
marioaugusto.martins <marioaugusto.martins@justeattakeaway.com>
2023-05-30 16:18:55 +0200

    Setup Hilt

--- app/src/main/java/com/mariommartins/sseclientflow/ui/MainActivity.kt
+++ app/src/main/java/com/mariommartins/sseclientflow/ui/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.mariommartins.sseclientflow.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
